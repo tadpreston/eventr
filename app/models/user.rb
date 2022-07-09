8510a52f8be504f5f885a37129e88507c162bbc4
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-09 08:41:36 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, confirmation: true
 end
