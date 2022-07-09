9a96c6f4c0b6f0eda7dcc34d164eb8817a0d9015
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-09 08:26:25 -0500

    Added first_name and last_name to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_193019) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_09_132510) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_193019) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
