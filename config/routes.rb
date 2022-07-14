9fbf2068a3d1939b4cd33e728ff5fa19530ceb06
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-14 13:22:15 -0500

    setup initial login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
+
   get "home/index"
   root "home#index"
 end
