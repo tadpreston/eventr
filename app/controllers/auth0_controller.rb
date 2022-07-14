c02331aaf1b525f72503653ac4aa3a8ed4c73c45
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-14 16:07:25 -0500

    added login and logout

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,6 +1,7 @@
 class Auth0Controller < ApplicationController
   def callback
     auth_info = request.env["omniauth.auth"]
+    byebug
     session[:userinfo] = auth_info["extra"]["raw_info"]
 
     redirect_to root_url
@@ -11,5 +12,21 @@ class Auth0Controller < ApplicationController
   end
 
   def logout
+    reset_session
+    redirect_to logout_url, allow_other_host: true
+  end
+
+  private
+
+  AUTH0_CONFIG = Rails.application.config.auth0
+
+  def logout_url
+
+    request_params = {
+      returnTo: root_url,
+      client_id: Rails.application.config.auth0['auth0_client_id']
+    }
+
+    URI::HTTPS.build(host: AUTH0_CONFIG['auth0_domain'], path: '/v2/logout', query: request_params.to_query).to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get "home/index"
+  root "home#index"
+
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
-
-  get "home/index"
-  root "home#index"
 end
