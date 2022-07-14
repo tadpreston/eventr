a9b71d4ac388d1cb72a07eadfa65a01840466054
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-14 09:03:14 -0500

    added default logic to callback controller

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,8 +1,13 @@
 class Auth0Controller < ApplicationController
   def callback
+    auth_info = request.env["omniauth.auth"]
+    session[:userinfo] = auth_info["extra"]["raw_info"]
+
+    redirect_to root_url
   end
 
   def failure
+    @error_msg = request.params['message']
   end
 
   def logout
