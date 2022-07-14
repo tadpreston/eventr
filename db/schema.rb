3bec0656e493b2fe2307c814403924e10b814ec0
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-14 16:22:24 -0500

    added new fields to the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_132429) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_211608) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
 end
