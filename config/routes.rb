5336e1d69c3252754daf34097726797314ddc3e1
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-16 12:21:37 -0500

    implemented token validation and the start of viewing/edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get "home/index"
-  root "home#index"
-
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
+
+  get 'profile', to: 'profile#show'
+  get 'profile/edit', to: 'profile#edit'
+  put 'profile', to: 'profile#update'
+
+  get "home/index"
+  root "home#index"
 end
