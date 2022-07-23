699d2a20a00b9f0d00f38d62e7d8e3416d69a9f6
Tad Preston <tad.preston@gatewaystaff.com>
2022-07-23 13:09:52 -0500

    created the phone_number table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_211608) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_17_195448) do
+  create_table "phone_numbers", force: :cascade do |t|
+    t.string "number"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_phone_numbers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_211608) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
+  add_foreign_key "phone_numbers", "users"
 end
